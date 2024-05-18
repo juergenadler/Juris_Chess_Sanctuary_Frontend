0c3ed143dd8aa0fe01c97fb15c9b354a2fc16e60
Juergen Adler <juergen.adler@gmx.net>
2024-05-18 20:04:38 +0200

    Initial checkin

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//
+// App.jsx
+//
+
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="t-h1">
+      Hello! This is App.jsx. Testing for Tailwind functionality.
+    </h1>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,html}",
+    "./index.html",
+    "./app.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

3a8fe255f20a1277f40b8270ceaa80d387541b73
Juergen Adler <juergen.adler@gmx.net>
2024-06-10 17:07:47 +0200

    Engine Output via SSE now functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
+
+import { EnvProvider } from './context/EnvContext.jsx';
 import ChessboardComponent from './components/ChessBoard.jsx';
 import EngineOutput from './components/EngineOutput.jsx';
+
 
 const App = () => {
   return (
     <div className="app-container flex">
       <div className="w-1/2 flex flex-col items-stretch">
         <ChessboardComponent />
-        <EngineOutput />
+        <EnvProvider>
+          <EngineOutput />
+        </EnvProvider>
       </div>
       <div className="right-half w-1/2">
 
--- src/components/EngineOutput.jsx
+++ src/components/EngineOutput.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
+
+import { useEnv } from '../context/EnvContext.jsx'; 
 import './EngineOutput.css';
 
+import { useEffect, useState } from "react";
+
 const EngineOutput = () => {
+  const { backEndSseApiUrl } = useEnv();
+  const [log, setLog] = useState([]);
+
+  useEffect(() => {
+    const eventSource = new EventSource(backEndSseApiUrl);
+
+    eventSource.onmessage = function (event) {
+      const data = JSON.parse(event.data);
+
+      // Append the data to the log state with the timestamp
+      let timestamp = new Date().toLocaleString(undefined, {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false      
+      });
+
+      timestamp = timestamp.replace(/\//g, '-');   
+      setLog((prevLog) => [...prevLog, `${timestamp}:  ${data}`]);
+    };
+
+    // Handle connection errors
+    eventSource.onerror = function (event) {
+      console.error("EventSource failed:", event);
+      eventSource.close();
+    };
+
+    // Cleanup on unmount
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
   return (
-    <textarea
-      className="textarea mt-4"
-      readOnly
-    />
+    <div className="container">
+      <h1 className="title">Stockfish Output</h1>
+      <textarea
+        readOnly
+        className="textarea"
+        value={log.join('\n')}
+      />
+    </div>
   );
 };
 
